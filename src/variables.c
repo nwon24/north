79c77565269f6cb373f678bc5ec53dfdebbb2c65
nwon24 <nathanw2047@gmail.com>
2022-12-23 12:04:39 +1100

    Code for variables

---
+++ src/variables.c
@@ -0,0 +1,142 @@
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "main.h"
+#include "variables.h"
+#include "hash.h"
+
+#define BSIZE 24
+
+#define HASH_SIZE 100
+
+Variable *variables = NULL;
+int nr_variables = 0;
+
+static HashTable *var_hash_table = NULL;;
+
+static struct {
+    char *name;
+    VariableType type;
+    int bytesize;
+} vartypes[] = {
+    { "i8", V_I8, 1 },
+    { "i16", V_I16, 2 },
+    { "i32", V_I32, 4 },
+    { "i64", V_I64, 8},
+    { "u8", V_U8, 1 },
+    { "u16", V_U16, 2 },
+    { "u32", V_U32, 4 },
+    { "u64", V_U64, 8 },
+    { "", V_UNKNOWN, 0 }
+};
+
+static VariableType str_to_vartype(char *str, int *bytesize);
+static void malloc_var(Variable *var);
+
+void init_variables_hash(void)
+{
+    var_hash_table = new_hash_table(string_hashfn, HASH_SIZE);
+}
+
+static void realloc_variables(void)
+{
+    int bsize = BSIZE;
+
+    if (variables == NULL) {
+	if ((variables = malloc(bsize * sizeof(*variables))) == NULL) {
+	    fatal("realloc_variables: malloc returned NULL");
+	}
+    } else {
+	if ((variables = realloc(variables, (nr_variables + bsize) * sizeof(*variables))) == NULL) {
+	    fatal("realloc_variables: realloc returned NULL");
+	}
+    }
+}
+
+static VariableType str_to_vartype(char *str, int *bytesize)
+{
+    int i = 0;
+
+    while (vartypes[i].type != V_UNKNOWN) {
+	if (strcmp(vartypes[i].name, str) == 0) {
+	    *bytesize = vartypes[i].bytesize;
+	    return vartypes[i].type;
+	}
+	i++;
+    }
+    return V_UNKNOWN;
+}
+
+static void malloc_var(Variable *var)
+{
+    var->addr = malloc(var->bytesize);
+    if (var->addr == NULL) {
+	fatal("malloc_var: malloc returned NULL");
+    }
+    memset(var->addr, 0, var->bytesize);
+}
+
+/*
+ * Syntax: .var <identifier> <type> <size>
+ */
+Token *add_variable(Token *start)
+{
+    Variable *newvar;
+    HashEntry *var_entry;
+    Token *tok;
+    int len;
+    int bytesize;
+
+    if (nr_variables % BSIZE == 0) {
+	realloc_variables();
+    }
+    newvar = &variables[nr_variables++];
+    assert(strcmp(start->text, ".var") == 0);
+    tok = start->next;
+    /* Identifier */
+    if (tok == NULL) {
+	tokerror(tok, ".var directive left hanging\n");
+    }
+    if ((len = strlen(tok->text)) > MAX_TOKEN_LENGTH) {
+	tokerror(tok, "Identifier '%s' too long\n", tok->text);
+    }
+    if (!isalpha(tok->text[0])) {
+	tokerror(tok, "Invalid identifier name '%s' - must begin with alphabetic character\n", tok->text);
+    }
+    var_entry = new_hash_entry(tok->text, newvar);
+    if (add_hash_entry(var_hash_table, var_entry) < 0) {
+	tokerror(tok, "Reuse of identifier '%s'\n");
+    }
+    strcpy(newvar->identifier, tok->text);
+
+    tok = tok->next;
+    /* Type */
+    if (tok == NULL) {
+	tokerror(tok, ".var directive left hanging\n");
+    }
+    bytesize = 0;
+    if ((newvar->type = str_to_vartype(tok->text, &bytesize)) == V_UNKNOWN) {
+	tokerror(tok, "Unknown variable type '%s'\n", tok->text);
+    }
+    /* Length */
+    tok = tok->next;
+    if (tok == NULL) {
+	tokerror(tok, ".var directive left hanging\n");
+    }
+    if ((newvar->length = atoi(tok->text)) == 0) {
+	tokerror(tok, "Invalid variable length/size '%s'\n", tok->text);
+    }
+    newvar->bytesize = newvar->length * bytesize;
+    if (simulating() == true) {
+	malloc_var(newvar);
+    }
+    return tok->next;
+}
+
+HashEntry *variable_reference(Token *tok)
+{
+    return in_hash(var_hash_table, tok->text);
+}
---
+++ src/variables.h
@@ -0,0 +1,35 @@
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#include <stdlib.h>
+
+#include "lex.h"
+#include "hash.h"
+
+typedef enum variable_type {
+    V_I64,
+    V_I32,
+    V_I16,
+    V_I8,
+    V_U64,
+    V_U32,
+    V_U16,
+    V_U8,
+    V_UNKNOWN,
+} VariableType;
+    
+typedef struct variable {
+    char identifier[MAX_TOKEN_LENGTH + 1];
+    VariableType type;
+    int length;
+    size_t bytesize;
+    void *addr; /* For simulation */
+} Variable;
+
+extern Variable *variables;
+extern int nr_variables;
+
+Token *add_variable(Token *start);
+HashEntry *variable_reference(Token *tok);
+
+#endif /* VARIABLES_H_ */
