a8828db75bbfdd29bfb119adb16667cae8769e6e
nwon24 <nathanw2047@gmail.com>
2022-12-30 19:28:10 +1100

    New testing framework! Not in Python!

---
+++ test.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+NORTH=./north
+
+run_cmd_test() {
+    expected=$(cat "$2")
+    actual=$($1)
+    if [ "$expected" = "$actual" ]; then
+	return 0
+    else
+	echo "Test failed."
+	printf "Expected\n%s but got\n%s" "$expected" "$actual"
+	return 1
+    fi
+
+}
+
+run_test() {
+    printf "Running simulation test on %s\n" "$1"
+    cmd="$NORTH -s $1"
+    run_cmd_test "$cmd" "$2"
+    printf "Running compilation test on %s\n" "$1"
+    cmd="$NORTH -rc $1"
+    run_cmd_test "$cmd" "$2"
+}
+
+
+#run_test "./tests/001-arithmetic/001-arithmetic.nth" "./tests/001-arithmetic/001-arithmetic.txt"
+
+success=0
+
+for dir in tests/*; do
+    run_test "$dir/"*.nth "$dir/"*.txt
+    if [ "$?" -eq 0 ]; then
+	success=$(($success+1))
+    fi
+done
+
+printf -- "---------------------------------------------------\n"
+printf "TEST RESULTS\n"
+printf "Passed: %s\n" "$success"
+printf -- "---------------------------------------------------\n"
+
