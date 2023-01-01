40f2d28906d29a6e6a1ffcaa8e825665f1c9eaed
nwon24 <nathanw2047@gmail.com>
2023-01-02 08:53:23 +1100

    Make symlink to correct syscall file

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@
 CC=${CC-cc}
 ARCH=${ARCH-x86_64-linux}
 
+case $ARCH in
+    x86_64-linux)
+	ln -svf x86_64/linux/syscall.nth stdlib/sysdeps/syscall.nth
+	;;
+    x86_64-freebsd)
+	ln -svf x86_64/freebsd/syscall.nth stdlib/sysdeps/syscall.nth
+	;;
+esac
+
 set +xe
 
 export CC
