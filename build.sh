bc0c924d993ce3f769734dd78cf3bdacdf7894e4
nwon24 <nathanw2047@gmail.com>
2023-01-03 17:36:16 +1100

    Be quite on ln command

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ ARCH=${ARCH-x86_64-linux}
 
 case $ARCH in
     x86_64-*)
-	ln -svf x86_64/x86_64.nth stdlib/sysdeps/arch.nth
+	ln -sf x86_64/x86_64.nth stdlib/sysdeps/arch.nth
 	;;
     x86_64-linux)
-	ln -svf x86_64/linux/syscall.nth stdlib/sysdeps/syscall.nth
+	ln -sf x86_64/linux/syscall.nth stdlib/sysdeps/syscall.nth
 	;;
     x86_64-freebsd)
-	ln -svf x86_64/freebsd/syscall.nth stdlib/sysdeps/syscall.nth
+	ln -sf x86_64/freebsd/syscall.nth stdlib/sysdeps/syscall.nth
 	;;
 esac
 
