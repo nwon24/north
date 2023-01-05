d7bbd73622dcdf66e116dcd1c8d39f9fa604e23f
Nathan Wong <nathanw2047@gmail.com>
2023-01-05 20:07:31 +1100

    Put typedef of word width in simulation header file

--- src/simulate.h
+++ src/simulate.h
@@ -1,6 +1,14 @@
 #ifndef SIMULATE_H
 #define SIMULATE_H
 
+#include <stdint.h>
+
+#if defined(__x86_64__)
+typedef int64_t word;
+#else
+#error "No other architecture currently supported for simulation"
+#endif
+
 extern char **simulated_argv;
 extern int simulated_argc;
 
